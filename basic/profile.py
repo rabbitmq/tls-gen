528ecaf8cfd1a47c768148661df884de9fc30d89
Luke Bakken <luke@bakken.io>
2019-10-31 14:33:47 -0700

    add option to gen client cert only

--- basic/profile.py
+++ basic/profile.py
@@ -6,7 +6,6 @@ import shutil
 
 def _copy_artifacts_to_results():
     os.makedirs(paths.relative_path("result"), exist_ok = True)
-
     gen.copy_root_ca_certificate_and_key_pair()
     gen.copy_leaf_certificate_and_key_pair("server")
     gen.copy_leaf_certificate_and_key_pair("client")
@@ -18,6 +17,13 @@ def generate(opts):
     gen.generate_server_certificate_and_key_pair(opts)
     gen.generate_client_certificate_and_key_pair(opts)
     _copy_artifacts_to_results()
+    print("Done! Find generated certificates and private keys under ./result!")
+
+def generate_client(opts):
+    cli.ensure_password_is_provided(opts)
+    print("Will generate a certificate/key pair (client only)")
+    gen.generate_client_certificate_and_key_pair(opts)
+    gen.copy_leaf_certificate_and_key_pair("client")
     print("Done! Find generated certificates and private keys under ./result!")
 
 def clean(opts):
@@ -51,14 +57,15 @@ def info(opts):
     info.leaf_certificate_info("client")
     info.leaf_certificate_info("server")
 
-commands = {"generate":   generate,
-            "gen":        generate,
-            "clean":      clean,
-            "regenerate": regenerate,
-            "regen":      regenerate,
-            "verify":     verify,
-            "verify-pkcs12": verify_pkcs12,
-            "info":       info}
+commands = {"generate":        generate,
+            "gen":             generate,
+            "generate-client": generate_client,
+            "clean":           clean,
+            "regenerate":      regenerate,
+            "regen":           regenerate,
+            "verify":          verify,
+            "verify-pkcs12":   verify_pkcs12,
+            "info":            info}
 
 if __name__ == "__main__":
     sys.path.append("..")
